133b93168ab36aa383e87d8b90f183ec48d40eed
Dajana11-99 <zlokapa.ra240.2018@uns.ac.rs>
2022-06-03 15:08:15 +0200

    fixed tests

---
+++ src/test/java/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
